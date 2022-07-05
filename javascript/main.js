fdceae6dbaa1da785f3e2123adc7fc6a2455d0ed
Manuel-Rodriguez-Pomares <rodriguezmanu6@gmail.com>
2022-07-05 18:52:05 -0300

    cambios

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 const Clickbutton = document.querySelectorAll('.button')
 const tbody = document.querySelector('.tbody')
-
+let carrito = [];
 
 
 Clickbutton.forEach(btn => {
@@ -158,7 +158,8 @@ window.onload = function(){
 fetch('https://my-json-server.typicode.com/Manuel-Rodriguez-Pomares/CoderHouse-ProyectoJavaScript-ManuelRodriguezPomares/lista')
 .then((response)=>response.json())
 .then((data)=>{
-
+   data = carrito
+   console.log(carrito)
 }) 
 
 
